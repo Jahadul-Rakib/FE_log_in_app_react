9daf156e746c3864174e115ebbd8b4a5923eb712
rakib <jhadul.islam@itconquest.com>
2020-05-14 09:40:25 +0600

    add

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color: '#666'}} className="row Title">Welcome</h1>
+    </div>
+  );
+}
+
+export default App;
